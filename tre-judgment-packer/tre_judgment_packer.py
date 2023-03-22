cb3c0c4954c161dde8a5b1d4c0723dafcfe59135
Alex Ford <alex.ford@nationalarchives.gov.uk>
2023-03-22 14:10:39 +0000

    Feat : updated packer function code to be able to handle anticipated message from judgment parser

--- tre-judgment-packer/tre_judgment_packer.py
+++ tre-judgment-packer/tre_judgment_packer.py
@@ -1,10 +1,12 @@
 import logging
 import boto3
+from botocore.exceptions import ClientError
 import s3_lib.object_lib
 from s3_lib import tar_lib
-from botocore.exceptions import ClientError
 from s3_lib import common_lib
+import time
 from tre_event_lib import tre_event_api
+import uuid
 
 # Set global logging options; AWS environment may override this though
 logging.basicConfig(
@@ -16,11 +18,11 @@ logger.setLevel(logging.INFO)
 
 # Env vars
 
+ENVIRONMENT = common_lib.get_env_var(
+    "TRE_ENVIRONMENT", must_exist=True, must_have_value=True
+)
 OUT_BUCKET = common_lib.get_env_var(
     "TRE_S3_JUDGMENT_OUT_BUCKET", must_exist=True, must_have_value=True
-)
-URL_EXPIRY = common_lib.get_env_var(
-    "TRE_PRESIGNED_URL_EXPIRY", must_exist=True, must_have_value=True
 )
 PROCESS = common_lib.get_env_var(
     "TRE_PROCESS_NAME", must_exist=True, must_have_value=True
@@ -28,23 +30,32 @@ PROCESS = common_lib.get_env_var(
 PRODUCER = common_lib.get_env_var(
     "TRE_SYSTEM_NAME", must_exist=True, must_have_value=True
 )
-ENVIRONMENT = common_lib.get_env_var(
-    "TRE_ENVIRONMENT", must_exist=True, must_have_value=True
+URL_EXPIRY = common_lib.get_env_var(
+    "TRE_PRESIGNED_URL_EXPIRY", must_exist=True, must_have_value=True
 )
 
-# Message Vars
+# Message in Vars
 
-KEY_S3_FOLDER_URL = "s3-folder-url"
-KEY_S3_SHA256_URL = "s3-sha256-url"
+KEY_S3_OBJECT_ROOT = "s3FolderName"
+ORIGINATOR = "originator"
+PARAMETERS = "parameters"
+PARENT_EXECUTION_ID = "parentExecutionId"
+PARSED_JUDGMENT_FILE_PATH = "s3FolderName"
+PROPERTIES = "properties"
+REFERENCE = "reference"
+SOURCE_BUCKET_NAME = "s3Bucket"
+
+# Message out Vars
+
+KEY_S3_FOLDER_URL = "bundleFileURI"
+META_DATA_FILE_PATH = "metadataFilePath"
+META_DATA_FILE_TYPE = "metadataFileType"
 
 # Lambda vars
 
-KEY_S3_OBJECT_ROOT = "s3FolderName"
-SOURCE_BUCKET_NAME = "s3Bucket"
-EVENT_NAME_INPUT = "tre-parsed-judgement"
-EVENT_NAME_OUTPUT_OK = "packed-judgment"
+EVENT_NAME_INPUT = "tre-judgment-parsed"
+EVENT_NAME_OUTPUT_OK = "tre-judgment-package-available"
 EVENT_NAME_OUTPUT_ERROR = "packed-judgment-error"
-PARSED_JUDGMENT_FILE_PATH = "parsed/judgment/"  # TODO check with Mark if this is going to the same place as currently?
 
 
 def handler(event, context):
@@ -55,35 +66,38 @@ def handler(event, context):
 
     logger.info(f'handler start: event="{event}"')
 
-    tre_event_api.validate_event(event=event, schema_name=EVENT_NAME_INPUT)
-    s3_source_bucket = event[tre_event_api.KEY_PARAMETERS][EVENT_NAME_INPUT][
-        tre_event_api.KEY_S3_BUCKET
-    ]
-    consignment_reference = event[tre_event_api.KEY_PARAMETERS][EVENT_NAME_INPUT][
-        tre_event_api.KEY_REFERENCE
-    ]
+    # Grabbing parameters from source event message.
+
+    s3_source_bucket = event[PARAMETERS][SOURCE_BUCKET_NAME]
+
+    reference = event[PARAMETERS][REFERENCE]
+
+    parsed_judgment_file_path = event[PARAMETERS][KEY_S3_OBJECT_ROOT]
+
+    parent_execution_id = event[PROPERTIES][PARENT_EXECUTION_ID]
 
     try:
 
         logger.info(
-            f"Packing consignment files {consignment_reference} from {s3_source_bucket} to {OUT_BUCKET}"
+            f"Packing files of ref: {reference} found at {parsed_judgment_file_path} in "
+            f"the {s3_source_bucket} and bundling to s3 bucket : {OUT_BUCKET}"
         )
 
         # Create a list of items to be zipped
-        # TODO check with Mark that the key for his parsed objects is just the consignment reference
 
         files_to_zip = s3_lib.object_lib.s3_ls(
-            bucket_name=SOURCE_BUCKET_NAME,
-            object_filter=f"{PARSED_JUDGMENT_FILE_PATH}{consignment_reference}/",
+            bucket_name=SOURCE_BUCKET_NAME, object_filter=f"{parsed_judgment_file_path}"
         )
+
+        # create a name for the package based on consigment reference
+
+        packed_judgment_file_name = f"{reference}.tar.gz"
+
+        # tar all the objects up into the package.
 
         logger.info(
-            f"packing {len(files_to_zip)} files from consignment {consignment_reference}"
+            f"packing {len(files_to_zip)} files in package {packed_judgment_file_name}"
         )
-
-        # Tar the objects up
-
-        packed_judgment_file_name = f".tar.gz"
 
         s3_lib.tar_lib.s3_objects_to_s3_tar_gz_file(
             s3_bucket_in=s3_source_bucket,
@@ -101,28 +115,39 @@ def handler(event, context):
                 Params={
                     "Bucket": OUT_BUCKET,
                     "Key": packed_judgment_file_name,
-                },  # TODO check file-naming convention
+                },
                 ExpiresIn=URL_EXPIRY,
+            )
+            logger.info(
+                f"packed file link generated for {packed_judgment_file_name} in {OUT_BUCKET}"
             )
         except ClientError as e:
             logging.error(e)
             return None
 
-        output_parameter_block = {
-            EVENT_NAME_OUTPUT_OK: {
-                tre_event_api.KEY_REFERENCE: consignment_reference,
-                KEY_S3_FOLDER_URL: s3_presigned_link,
-            }
-        }
+        # produce timestamp for message
 
-        event_output_success = tre_event_api.create_event(
-            environment=ENVIRONMENT,
-            producer=PRODUCER,
-            process=PROCESS,
-            event_name=EVENT_NAME_OUTPUT_OK,
-            prior_event=event,
-            parameters=output_parameter_block,
-        )
+        timestamp_ns_utc = time.time_ns()
+
+        # produce UUID for message
+
+        event_uuid = str(uuid.uuid4())
+
+        event_output_success = {
+            "properties": {
+                "messageType": "uk.gov.nationalarchives.tre.messages.packed.judgment.JudgmentPacked",
+                "timestampMillis": timestamp_ns_utc,
+                "function": PROCESS,
+                "producer": PRODUCER,
+                "executionId": event_uuid,
+                "parentExecutionId": parent_execution_id,
+            },
+            "parameters": {
+                "bundleFileURI": s3_presigned_link,
+                "metadataFilePath": "/metadata.json",
+                "metadataFileType": "Json",
+            },
+        }
 
         logger.info(f"event_output_success:\n%s\n", event_output_success)
         return event_output_success
@@ -131,7 +156,7 @@ def handler(event, context):
         logging.error("handler error: %s", str(e))
         output_parameter_block = {
             EVENT_NAME_OUTPUT_ERROR: {
-                tre_event_api.KEY_REFERENCE: consignment_reference,
+                tre_event_api.KEY_REFERENCE: reference,
                 tre_event_api.KEY_ERRORS: [str(e)],
             }
         }
