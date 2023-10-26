1832c079ac884c7d4546df202d8cdeaf8de93bdd
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-10-26 10:32:57 +0100

    fix: Add TRE prefix to v2 output package (#15)

--- tre-judgment-packer/tre_judgment_packer.py
+++ tre-judgment-packer/tre_judgment_packer.py
@@ -75,10 +75,12 @@ def handler(event, context):
             bucket_name=s3_source_bucket, object_filter=f"{parsed_judgment_file_path}"
         )
 
+        tre_internal_reference = f"TRE-{reference}"
+
         # construct file path
         s3_key_file_path = f"{reference}/{execution_id}/"
         # create a name for the package based on reference
-        packed_judgment_file_name = f"{reference}.tar.gz"
+        packed_judgment_file_name = f"{tre_internal_reference}.tar.gz"
         # s3 object key with name
         s3_file_path_with_file_name = f"{s3_key_file_path}{packed_judgment_file_name}"
 
